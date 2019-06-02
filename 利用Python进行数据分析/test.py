0316478d0a58d231f5e0831497de99325e767e68
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-02 14:45:34 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -9,26 +9,23 @@ __Class_Path__=MyClass_Path("\\利用Python进行数据分析")
 from MyPackage.MyClass_Plot import MyClass_Figure
 Class_Figure=MyClass_Figure()
 #------------------------------------------
-
-a = np.array([[1, 2], [3, np.nan]])
-sum(a)
-np.sum(a,0)
-np.nansum(a)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+m=np.ones([3,3])
+a=np.array([0,1,1])
+b=np.array([0,1,0])
+a | b
+b=np.array([[1]])
+np.add(a,m)
+m+b
+c=np.array([[1]])
+m+c
+d=np.array([[[1],2]])
+m+d
+x=np.linspace(0,5,50)
+y=np.linspace(0,5,50)[:,np.newaxis]
+z=np.sin(x)**10+np.cos(10+y*x)*np.cos(x)
+z
+plt.imshow(z)
+plt.show()
 
 
 
