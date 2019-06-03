f7f5d8bc55821386d41cae3546da3e15b577d88d
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-03 17:18:31 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -10,19 +10,9 @@ myplt=MyPackage.MyClass_Plot.MyClass_Figure()
 mynp=MyPackage.MyClass_Array.MyClass_NumPy()
 #------------------------------------------
 a=mynp.ndarray([i for i in range(10)])
-b=mynp.gen_appoint(10)
-c=mynp.gen_range(0,9,1,shape=(1,3,3))
-c.shape, c.ndim, c.dtype,c
-b.dtype=np.dtype("i2")
-b.astype("int64")
+b=mynp.gen_appoint((5,5))
 b
-c
-c.astype("S10")
-c
-a=mynp.ndarray(["adf","34b","fdfc","DDF","EEF","GEF"])
-a.astype("U1")
-
-
+b[0]=[1,2,3,4,5]
 
 
 
