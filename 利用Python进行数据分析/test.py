09e36abd754fb8dbd06d2d1a84eaa8a65b0a5b1d
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-05 17:41:35 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -7,39 +7,13 @@ import statsmodels as sm
 import MyPackage
 __mypath__=MyPackage.MyClass_Path.MyClass_Path("\\利用Python进行数据分析")
 myplt=MyPackage.MyClass_Plot.MyClass_Figure()
+mypltpro=MyPackage.MyClass_PlotPro.MyClass_PlotPro()
 mynp=MyPackage.MyClass_Array.MyClass_NumPy()
+
 #------------------------------------------
-#---对外接口：k值，axesindex，N*2结构[[x1,y1],[x2,y2]...]的数据
-k=3
-axesindex=0
-#---接受N*2的数据结构[[x1,y1],[x2,y2]...]
-x=mynp.gen_random(shape=(10,2))
-myplt.PlotScatter(0,x[:,0],x[:,1])
+#---K个最邻近点
+mypltpro.KNearest(2,mynp.gen_random(shape=(20,2)),axesindex=0,OnlyScatter=False)
 
-#---批量生成距离差，每个点都匹配
-diff=x[:,np.newaxis,:]-x #注意减号前后不同，结果意义不同
-#---SquareDistance为平方距离
-SquareDistance=(diff**2).sum(axis=2)
-#---获得距离，数据意义：[[p0-p0,p0-p1...],[p1-p0,p1-p1...],...]
-Distance=SquareDistance**0.5
-
-#---获得排序序号
-nearest=np.argsort(Distance,axis=1)
-
-#---获得k个最邻近的序号(k个最小值分割到前面)
-nearest_partition=np.argpartition(Distance,k+1,axis=1)
-#---清理指定的axes
-myplt.ClearAxes(axesindex)
-#---画图
-for i in range(x.shape[0]):
-    for j in nearest_partition[i,:k+1]:
-        #画从x[i]到x[j]的线段(x[i]为点坐标)
-        myplt.PlotLine2Dot(axesindex,x[i],x[j],"",cla=False,show=False)
-myplt.FigureShow()
-
-
-
-
-
+input("")
 
 
