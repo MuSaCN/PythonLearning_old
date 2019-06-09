826ce671630b5be1b1120041c417ed4db37606a4
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-09 19:05:38 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -9,32 +9,10 @@ __mypath__=MyPackage.MyClass_Path.MyClass_Path("\\利用Python进行数据分析
 myplt=MyPackage.MyClass_Plot.MyClass_Figure()
 mypltpro=MyPackage.MyClass_PlotPro.MyClass_PlotPro()
 mynp=MyPackage.MyClass_Array.MyClass_NumPy()
-
+mypd=MyPackage.MyClass_Array.MyClass_Pandas()
 #------------------------------------------
-#---K个最邻近点
-#mypltpro.KNearest(2,mynp.gen_random("random",-500,500,shape=(100,2)),axesindex=0,OnlyScatter=False)
-myplt.__init__()
-nwalks=5000
-nsteps=1000
-draws=mynp.gen_random("randint",0,1,shape=(nwalks,nsteps))
-steps=mynp.where(draws>0,1,-1)
-walk=steps.cumsum()
-myplt.PlotLine2D(0,walk)
-walk.min()
-walk.max()
-(mynp.abs(walk) >=10).argmax()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+a=mypd.Index([1,2,3,4,5,"a","b","c"])
+a
+mypd.ChangeIndex(a,"delete","b")
 
