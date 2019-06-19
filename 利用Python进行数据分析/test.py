f6cb0b9db8e5b81070f55ecf9bbc90b3c291b5b3
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-19 23:10:02 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -16,6 +16,40 @@ path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析
 
 
 
+#%%
+s1 = pd.Series([0, 1], index=['a', 'b'])
+s2 = pd.Series([2, 3, 4], index=['c', 'd', 'e'])
+s3 = pd.Series([5, 6], index=['f', 'g'])
+df1 = pd.Series([0, 1], index=['a', 'b'])
+df2 = pd.Series([2, 3, 4], index=['c', 'd', 'e'])
+df3 = pd.Series([5, 6], index=['f', 'g'])
 
+
+#%%
+result = mypd.concat([s1, s2, s3], keys=['one', 'two', 'three'])
+result
+result.unstack()
+#%%
+mypd.concat([df1, df2, df3], axis=1, keys=['one', 'two', 'three'])
+#%%
+df1 = pd.DataFrame(np.arange(6).reshape(3, 2), index=['a', 'b', 'c'],
+                   columns=['one', 'two'])
+df2 = pd.DataFrame(5 + np.arange(4).reshape(2, 2), index=['a', 'c'],
+                   columns=['three', 'four'])
+df1
+df2
+mypd.concat([df1, df2], axis=1, keys=['level1', 'level2'])
+#%%
+mypd.concat({'level1': df1, 'level2': df2}, axis=1)
+#%%
+mypd.concat([df1, df2], axis=1, keys=['level1', 'level2'],
+          names=['upper', 'lower'])
+#%%
+df1 = pd.DataFrame(np.random.randn(3, 4), columns=['a', 'b', 'c', 'd'])
+df2 = pd.DataFrame(np.random.randn(2, 3), columns=['b', 'd', 'a'])
+df1
+df2
+#%%
+mypd.concat([df1, df2], axis=1,ignore_index=True,keys=["A","B"],names=["aa","bb"])
 
 
