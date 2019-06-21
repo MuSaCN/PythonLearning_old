eb772c80bf4b6cf0023547be81354e5f4c2b3032
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-21 20:37:21 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -12,5 +12,76 @@ mynp=MyPackage.MyClass_Array.MyClass_NumPy()
 mypd=MyPackage.MyClass_Array.MyClass_Pandas()
 myfile=MyPackage.MyClass_File.MyClass_File()
 #------------------------------------------
-path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\ch02"
+path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\"
+
+
+#%%
+df = pd.DataFrame({'key': ['foo', 'bar', 'baz'],
+                   'A': [1, 2, 3],
+                   'B': [4, 5, 6],
+                   'C': [7, 8, 9]})
+df
+#%%
+melted = pd.melt(df, ['key'])
+melted1=mypd.PivotMelt(df,"melt",id_vars= ['key'],value_vars=None)
+mypd.PivotMelt(melted1,"pivot","key","variable","value")
+melted.pivot('key', 'variable', 'value')
+melted
+#%%
+reshaped = melted.pivot('key', 'variable', 'value')
+reshaped
+#%%
+reshaped.reset_index()
+#%%
+pd.melt(df, id_vars=['key'], value_vars=['A', 'B'])
+#%%
+pd.melt(df, value_vars=['A', 'B', 'C'])
+pd.melt(df, value_vars=['key', 'A', 'B'])
+#%% md
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
