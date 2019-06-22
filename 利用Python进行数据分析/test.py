9a3b5d68a2c35c38178bf01672fa53e550a52a39
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-22 18:55:21 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import seaborn as sns
 import statsmodels as sm
 import MyPackage
@@ -13,17 +14,112 @@ mypd=MyPackage.MyClass_Array.MyClass_Pandas()
 myfile=MyPackage.MyClass_File.MyClass_File()
 #------------------------------------------
 path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\"
-a=myfile.read_pd(path+"examples\\macrodata.csv")
-a
 
 
-
-
-
-
-
-
-
+#%%
+s = pd.Series(np.random.randn(10).cumsum(), index=np.arange(0, 100, 10))
+s.plot()
+#%%
+df = pd.DataFrame(np.random.randn(10, 4).cumsum(0),
+                  columns=['A', 'B', 'C', 'D'],
+                  index=np.arange(0, 100, 10))
+df.plot()
+#%% md
+### Bar Plots
+#%%
+fig, axes = plt.subplots(2, 1)
+data = pd.Series(np.random.rand(16), index=list('abcdefghijklmnop'))
+data.plot.bar(ax=axes[0], color='k', alpha=0.7)
+data.plot.barh(ax=axes[1], color='k', alpha=0.7)
+#%%
+np.random.seed(12348)
+#%%
+df = pd.DataFrame(np.random.rand(6, 4),
+                  index=['one', 'two', 'three', 'four', 'five', 'six'],
+                  columns=pd.Index(['A', 'B', 'C', 'D'], name='Genus'))
+df
+df.plot.bar()
+#%%
+plt.figure()
+#%%
+df.plot.barh(stacked=True, alpha=0.5)
+#%%
+plt.close('all')
+#%%
+tips = pd.read_csv('examples/tips.csv')
+party_counts = pd.crosstab(tips['day'], tips['size'])
+party_counts
+# Not many 1- and 6-person parties
+party_counts = party_counts.loc[:, 2:5]
+#%%
+# Normalize to sum to 1
+party_pcts = party_counts.div(party_counts.sum(1), axis=0)
+party_pcts
+party_pcts.plot.bar()
+#%%
+plt.close('all')
+#%%
+import seaborn as sns
+tips['tip_pct'] = tips['tip'] / (tips['total_bill'] - tips['tip'])
+tips.head()
+sns.barplot(x='tip_pct', y='day', data=tips, orient='h')
+#%%
+plt.close('all')
+#%%
+sns.barplot(x='tip_pct', y='day', hue='time', data=tips, orient='h')
+#%%
+plt.close('all')
+#%%
+sns.set(style="whitegrid")
+#%% md
+### Histograms and Density Plots
+#%%
+plt.figure()
+#%%
+tips['tip_pct'].plot.hist(bins=50)
+#%%
+plt.figure()
+#%%
+tips['tip_pct'].plot.density()
+#%%
+plt.figure()
+#%%
+comp1 = np.random.normal(0, 1, size=200)
+comp2 = np.random.normal(10, 2, size=200)
+values = pd.Series(np.concatenate([comp1, comp2]))
+sns.distplot(values, bins=100, color='k')
+#%% md
+### Scatter or Point Plots
+#%%
+macro = pd.read_csv('examples/macrodata.csv')
+data = macro[['cpi', 'm1', 'tbilrate', 'unemp']]
+trans_data = np.log(data).diff().dropna()
+trans_data[-5:]
+#%%
+plt.figure()
+#%%
+sns.regplot('m1', 'unemp', data=trans_data)
+plt.title('Changes in log %s versus log %s' % ('m1', 'unemp'))
+#%%
+sns.pairplot(trans_data, diag_kind='kde', plot_kws={'alpha': 0.2})
+#%% md
+### Facet Grids and Categorical Data
+#%%
+sns.factorplot(x='day', y='tip_pct', hue='time', col='smoker',
+               kind='bar', data=tips[tips.tip_pct < 1])
+#%%
+sns.factorplot(x='day', y='tip_pct', row='time',
+               col='smoker',
+               kind='bar', data=tips[tips.tip_pct < 1])
+#%%
+sns.factorplot(x='tip_pct', y='day', kind='box',
+               data=tips[tips.tip_pct < 0.5])
+#%% md
+## Other Python Visualization Tools
+#%%
+pd.options.display.max_rows = PREVIOUS_MAX_ROWS
+#%% md
+## Conclusion
 
 
 
