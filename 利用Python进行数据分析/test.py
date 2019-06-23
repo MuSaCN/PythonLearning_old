a8ee742d51281d5f507757bff610fcfa7ccf1582
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-23 19:50:23 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -17,67 +17,20 @@ mypd=MyPackage.MyClass_Array.MyClass_Pandas()           #矩阵数组类(整合P
 path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\"
 
 
-#%%
-s = pd.Series(np.random.randn(10).cumsum(), index=np.arange(0, 100, 10))
-
-myplt.PlotLine2D(0,s,cla=True)
-
-s.plot()
-plt.show()
-#%%
-df = pd.DataFrame(np.random.randn(10, 4).cumsum(0),columns=['A', 'B', 'C', 'D'],index=np.arange(0, 100, 10))
-df.plot.area(subplots=True)
-plt.show()
-
-nn=np.array(np.random.randn(10, 4))
-nn.tolist()
-
-myplt.PlotLine2D(0,df,cla=True)
-
-df.plot(subplots=True)
-#%% md
-### Bar Plots
-#%%
-fig, axes = plt.subplots(2, 1)
-data = pd.Series(np.random.rand(16), index=list('abcdefghijklmnop'))
-data.plot.bar(ax=axes[0], color='k', alpha=0.7)
-data.plot.barh(ax=axes[1], color='k', alpha=0.7)
-#%%
-np.random.seed(12348)
-#%%
-df = pd.DataFrame(np.random.rand(6, 4),
-                  index=['one', 'two', 'three', 'four', 'five', 'six'],
-                  columns=pd.Index(['A', 'B', 'C', 'D'], name='Genus'))
-df
-df.plot.bar()
-#%%
-plt.figure()
-#%%
-df.plot.barh(stacked=True, alpha=0.5)
-#%%
-plt.close('all')
-#%%
-tips = pd.read_csv('examples/tips.csv')
+tips = pd.read_csv(path+'examples\\tips.csv')
 party_counts = pd.crosstab(tips['day'], tips['size'])
-party_counts
-# Not many 1- and 6-person parties
 party_counts = party_counts.loc[:, 2:5]
-#%%
-# Normalize to sum to 1
 party_pcts = party_counts.div(party_counts.sum(1), axis=0)
-party_pcts
-party_pcts.plot.bar()
-#%%
-plt.close('all')
-#%%
-import seaborn as sns
 tips['tip_pct'] = tips['tip'] / (tips['total_bill'] - tips['tip'])
-tips.head()
-sns.barplot(x='tip_pct', y='day', data=tips, orient='h')
+
 #%%
-plt.close('all')
-#%%
-sns.barplot(x='tip_pct', y='day', hue='time', data=tips, orient='h')
+sns.barplot(x='day', y='tip_pct', hue='time', data=tips)
+myfig.ReSetFigureAxes()
+myfig.PlotBar_distribution(0,tips,x='day', y='tip_pct',hue='time',ci="sd")
+
+
+
+plt.show()
 #%%
 plt.close('all')
 #%%
