a5db5e0f8d2cc380e610bd2f3d89447b0e5d1f28
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-23 12:38:34 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -6,24 +6,35 @@ import matplotlib.patches as patches
 import seaborn as sns
 import statsmodels as sm
 import MyPackage
-__mypath__=MyPackage.MyClass_Path.MyClass_Path("\\利用Python进行数据分析")
-myplt=MyPackage.MyClass_Plot.MyClass_Figure()
-mypltpro=MyPackage.MyClass_PlotPro.MyClass_PlotPro()
-mynp=MyPackage.MyClass_Array.MyClass_NumPy()
-mypd=MyPackage.MyClass_Array.MyClass_Pandas()
-myfile=MyPackage.MyClass_File.MyClass_File()
-#------------------------------------------
+__mypath__=MyPackage.MyClass_Path.MyClass_Path("\\利用Python进行数据分析") #路径类
+myfile=MyPackage.MyClass_File.MyClass_File()            #文件操作类
+myplt=MyPackage.MyClass_Plot.MyClass_Plot()             #直接绘图类(单个图窗)
+myfig=MyPackage.MyClass_Plot.MyClass_Figure()           #对象式绘图类(可多个图窗)
+myfigpro=MyPackage.MyClass_PlotPro.MyClass_FigurePro()  #高级对象式绘图类
+mynp=MyPackage.MyClass_Array.MyClass_NumPy()            #多维数组类(整合Numpy)
+mypd=MyPackage.MyClass_Array.MyClass_Pandas()           #矩阵数组类(整合Pandas)
+#---------------------------------------------------------
 path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\"
 
 
 #%%
 s = pd.Series(np.random.randn(10).cumsum(), index=np.arange(0, 100, 10))
+
+myplt.PlotLine2D(0,s,cla=True)
+
 s.plot()
+plt.show()
 #%%
-df = pd.DataFrame(np.random.randn(10, 4).cumsum(0),
-                  columns=['A', 'B', 'C', 'D'],
-                  index=np.arange(0, 100, 10))
-df.plot()
+df = pd.DataFrame(np.random.randn(10, 4).cumsum(0),columns=['A', 'B', 'C', 'D'],index=np.arange(0, 100, 10))
+df.plot.area(subplots=True)
+plt.show()
+
+nn=np.array(np.random.randn(10, 4))
+nn.tolist()
+
+myplt.PlotLine2D(0,df,cla=True)
+
+df.plot(subplots=True)
 #%% md
 ### Bar Plots
 #%%
