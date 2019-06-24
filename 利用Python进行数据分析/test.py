cad4cb21bb1a453eeb25744adae63796b5df215d
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-06-24 16:02:55 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -16,7 +16,6 @@ mypd=MyPackage.MyClass_Array.MyClass_Pandas()           #矩阵数组类(整合P
 #---------------------------------------------------------
 path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\"
 
-myfigpro.ReSetFigureAxes(1,1)
 
 tips = pd.read_csv(path+'examples\\tips.csv')
 party_counts = pd.crosstab(tips['day'], tips['size'])
@@ -24,49 +23,17 @@ party_counts = party_counts.loc[:, 2:5]
 party_pcts = party_counts.div(party_counts.sum(1), axis=0)
 tips['tip_pct'] = tips['tip'] / (tips['total_bill'] - tips['tip'])
 
-myfigpro.ScatterMatrix(tips,reg=True,density=True,show=False)
-myfigpro.PlotSave("ScatterMatrix.jpg")
-
-
-sns.pairplot(tips,hue="time",diag_kind="kde")
-sns.pairplot(tips,hue="time",diag_kind="kde",kind="reg")
-sns.pairplot(tips, kind="reg",ax=myfig.AxesList[0])
-plt.show()
-
-
 macro = pd.read_csv(path+'examples\\macrodata.csv')
 data = macro[['cpi', 'm1', 'tbilrate', 'unemp']]
 trans_data = np.log(data).diff().dropna()
-trans_data[-5:]
+a=tips[tips.tip_pct < 1]
 
-myfigpro.ScatterAndRegression(trans_data['m1'],trans_data['unemp'])
+myfigpro.CategoricalPlot(x="day",y="tip_pct",hue="time",dataarray=a,col="smoker",kind="bar",ci="sd")
 
-
-#%%
-sns.pairplot(trans_data, diag_kind='kde', plot_kws={'alpha': 0.2})
-sns.pairplot(trans_data,hue="cpi")
-plt.show()
+myfigpro.CategoricalPlot(x="day",y="tip_pct",hue="time",dataarray=a,col="smoker",kind="bar",ci=95)
 
 
 
-#%% md
-### Facet Grids and Categorical Data
-#%%
-sns.factorplot(x='day', y='tip_pct', hue='time', col='smoker',
-               kind='bar', data=tips[tips.tip_pct < 1])
-#%%
-sns.factorplot(x='day', y='tip_pct', row='time',
-               col='smoker',
-               kind='bar', data=tips[tips.tip_pct < 1])
-#%%
-sns.factorplot(x='tip_pct', y='day', kind='box',
-               data=tips[tips.tip_pct < 0.5])
-#%% md
-## Other Python Visualization Tools
-#%%
-pd.options.display.max_rows = PREVIOUS_MAX_ROWS
-#%% md
-## Conclusion
 
 
 
