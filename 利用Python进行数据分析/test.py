50017d4f6d463299f23e68424651b9a2d856461a
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-07-07 19:56:44 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -21,67 +21,40 @@ path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析
 #%% md
 ## Moving Window Functions
 #%%
-close_px_all = pd.read_csv('examples/stock_px_2.csv',
-                           parse_dates=True, index_col=0)
+close_px_all = pd.read_csv(path+'examples\\stock_px_2.csv',parse_dates=True, index_col=0)
 close_px = close_px_all[['AAPL', 'MSFT', 'XOM']]
 close_px = close_px.resample('B').ffill()
-#%%
-close_px.AAPL.plot()
-close_px.AAPL.rolling(250).mean().plot()
-#%%
-plt.figure()
-#%%
-appl_std250 = close_px.AAPL.rolling(250, min_periods=10).std()
-appl_std250[5:12]
-appl_std250.plot()
-#%%
+
+appl_std250 = close_px.AAPL.rolling(250, min_periods=None).std()
+
 expanding_mean = appl_std250.expanding().mean()
-#%%
-plt.figure()
-#%%
-close_px.rolling(60).mean().plot(logy=True)
-#%%
-close_px.rolling('20D').mean()
-#%% md
-### Exponentially Weighted Functions
-#%%
-plt.figure()
-#%%
+
 aapl_px = close_px.AAPL['2006':'2007']
 ma60 = aapl_px.rolling(30, min_periods=20).mean()
 ewma60 = aapl_px.ewm(span=30).mean()
-ma60.plot(style='k--', label='Simple MA')
-ewma60.plot(style='k-', label='EW MA')
-plt.legend()
+
 #%% md
 ### Binary Moving Window Functions
 #%%
-plt.figure()
+
 #%%
 spx_px = close_px_all['SPX']
 spx_rets = spx_px.pct_change()
 returns = close_px.pct_change()
-#%%
+
 corr = returns.AAPL.rolling(125, min_periods=100).corr(spx_rets)
-corr.plot()
-#%%
-plt.figure()
-#%%
-corr = returns.rolling(125, min_periods=100).corr(spx_rets)
-corr.plot()
-#%% md
-### User-Defined Moving Window Functions
-#%%
-plt.figure()
+
 #%%
 from scipy.stats import percentileofscore
 score_at_2percent = lambda x: percentileofscore(x, 0.02)
-result = returns.AAPL.rolling(250).apply(score_at_2percent)
+result = returns.rolling(250).apply(score_at_2percent)
+
+mypdpro.TimeSeries_PercentileOfScore(returns,250,0.02).plot()
+
+
 result.plot()
+plt.show()
 #%%
-pd.options.display.max_rows = PREVIOUS_MAX_ROWS
-#%% md
-## Conclusion
 
 
 
