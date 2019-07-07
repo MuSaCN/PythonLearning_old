fdfe011437f22700b876c38a585fa00dd986c522
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-07-07 19:57:26 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -18,43 +18,6 @@ mytime=MyPackage.MyClass_Time.MyClass_Time()            #时间类
 #---------------------------------------------------------
 path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\"
 
-#%% md
-## Moving Window Functions
-#%%
-close_px_all = pd.read_csv(path+'examples\\stock_px_2.csv',parse_dates=True, index_col=0)
-close_px = close_px_all[['AAPL', 'MSFT', 'XOM']]
-close_px = close_px.resample('B').ffill()
-
-appl_std250 = close_px.AAPL.rolling(250, min_periods=None).std()
-
-expanding_mean = appl_std250.expanding().mean()
-
-aapl_px = close_px.AAPL['2006':'2007']
-ma60 = aapl_px.rolling(30, min_periods=20).mean()
-ewma60 = aapl_px.ewm(span=30).mean()
-
-#%% md
-### Binary Moving Window Functions
-#%%
-
-#%%
-spx_px = close_px_all['SPX']
-spx_rets = spx_px.pct_change()
-returns = close_px.pct_change()
-
-corr = returns.AAPL.rolling(125, min_periods=100).corr(spx_rets)
-
-#%%
-from scipy.stats import percentileofscore
-score_at_2percent = lambda x: percentileofscore(x, 0.02)
-result = returns.rolling(250).apply(score_at_2percent)
-
-mypdpro.TimeSeries_PercentileOfScore(returns,250,0.02).plot()
-
-
-result.plot()
-plt.show()
-#%%
 
 
 
