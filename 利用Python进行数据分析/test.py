0a316e9b781a7ca65c6297c47f5ebef8376c5b22
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-07-18 17:08:30 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -18,74 +18,12 @@ mytime=MyPackage.MyClass_Time.MyClass_Time()            #时间类
 #---------------------------------------------------------
 path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\"
 path1="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\Python数据科学手册\\notebooks\\"
-from mpl_toolkits import mplot3d
 
-myfig.SetAxes_3D2D()
-myfig.FigureShow()
-
-myfig.AxesList[0].plot3D(xline, yline, zline)
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
 
 
-# Data for a three-dimensional line
-zline = np.linspace(0, 15, 1000)
-xline = np.sin(zline)
-yline = np.cos(zline)
-ax.plot3D(xline, yline, zline, 'gray')
-
-# Data for three-dimensional scattered points
-zdata = 15 * np.random.random(100)
-xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
-ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
-ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
-
-def f(x, y):
-    return np.sin(np.sqrt(x ** 2 + y ** 2))
-
-x = np.linspace(-6, 6, 30)
-y = np.linspace(-6, 6, 30)
-
-X, Y = np.meshgrid(x, y)
-Z = f(X, Y)
-#%%
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.contour3D(X, Y, Z, 50, cmap='binary')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z');
-ax.view_init(60, 35)
-fig
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.plot_wireframe(X, Y, Z, color='black')
-ax.set_title('wireframe');
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                cmap='viridis', edgecolor='none')
-ax.set_title('surface');
-r = np.linspace(0, 6, 20)
-theta = np.linspace(-0.9 * np.pi, 0.8 * np.pi, 40)
-r, theta = np.meshgrid(r, theta)
-
-X = r * np.sin(theta)
-Y = r * np.cos(theta)
-Z = f(X, Y)
-
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                cmap='viridis', edgecolor='none');
-theta = 2 * np.pi * np.random.random(1000)
-r = 6 * np.random.random(1000)
-x = np.ravel(r * np.sin(theta))
-y = np.ravel(r * np.cos(theta))
-z = f(x, y)
-
-ax = plt.axes(projection='3d')
-ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=0.5);
-ax = plt.axes(projection='3d')
-ax.plot_trisurf(x, y, z,
-                cmap='viridis', edgecolor='none');
 
 myfig.ReSetFigureAxes()
 # myfig.AxesList[0].
