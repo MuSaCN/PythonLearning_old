5b82556ffec2749ec894bd689abb13906cdf2edf
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-07-18 20:45:54 +0800

    update

--- 利用Python进行数据分析/test.py
+++ 利用Python进行数据分析/test.py
@@ -19,21 +19,32 @@ mytime=MyPackage.MyClass_Time.MyClass_Time()            #时间类
 path="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\利用Python进行数据分析(第二版)代码\\"
 path1="C:\\Users\\i2011\\OneDrive\\Book_Code&Data\\Python数据科学手册\\notebooks\\"
 
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-
-
-
 myfig.ReSetFigureAxes()
 # myfig.AxesList[0].
 # myfig.fig.
 myfig.FigureShow()
 plt.show()
 
+rng = np.random.RandomState(0)
+x = np.linspace(0, 10, 500)
+y = np.cumsum(rng.randn(500, 6), 0)
+plt.plot(x, y)
+plt.legend('ABCDEF', ncol=2, loc='upper left');
+
+import seaborn as sns
+
+
+tips = sns.load_dataset('tips')
+tips.head()
+tips['tip_pct'] = 100 * tips['tip'] / tips['total_bill']
+
+
+myfigpro.FacetGrid_hist(tips,"sex","time","tip_pct",bins=15)
 
 
 
-
+grid = sns.FacetGrid(tips, row="sex", col="time", margin_titles=True)
+grid.map(plt.hist, "tip_pct", bins=15);
+plt.show()
 
 
