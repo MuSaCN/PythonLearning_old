d6c6889dc12e418b67b8e7c78269398e4abde4ab
MuSaCN <435116098@qq.com>
2019-09-03 12:32:32 +0800

    update

---
+++ 量化投资以Python为工具/chapter13.py
@@ -0,0 +1,30 @@
+# Author:Zhang Yuan
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+import statsmodels as sm
+import MyPackage
+
+__mypath__ = MyPackage.MyClass_Path.MyClass_Path("\\量化投资以Python为工具")  #路径类
+myfile = MyPackage.MyClass_File.MyClass_File()  #文件操作类
+myplt = MyPackage.MyClass_Plot.MyClass_Plot()  #直接绘图类(单个图窗)
+myfig = MyPackage.MyClass_Plot.MyClass_Figure()  #对象式绘图类(可多个图窗)
+myfigpro = MyPackage.MyClass_PlotPro.MyClass_FigurePro()  #高级对象式绘图类
+mynp = MyPackage.MyClass_Array.MyClass_NumPy()  #多维数组类(整合Numpy)
+mypd = MyPackage.MyClass_Array.MyClass_Pandas()  #矩阵数组类(整合Pandas)
+mypdpro = MyPackage.MyClass_ArrayPro.MyClass_PandasPro()  #高级矩阵数组类
+mytime = MyPackage.MyClass_Time.MyClass_Time()  #时间类
+
+
+
+
+
+
+
+
+
+
+
+
