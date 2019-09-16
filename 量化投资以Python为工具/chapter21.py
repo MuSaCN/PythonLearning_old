8ebf4bbe633e171e0683191d95c5d044bcc53840
MuSa <39754824+MuSaCN@users.noreply.github.com>
2019-09-16 18:41:16 +0800

    update

--- 量化投资以Python为工具/chapter21.py
+++ 量化投资以Python为工具/chapter21.py
@@ -30,55 +30,40 @@ import pandas_datareader.data as web
 import datetime as dt
 import statsmodels.formula.api as smf
 from statsmodels.api import add_constant
-
+import statsmodels.api as sm
 # 1.
-wanke = web.DataReader('000002.SZ', 'yahoo',
-                       dt.datetime(2015, 1, 1), dt.datetime(2015, 12, 31))
-
-gldc = web.DataReader('600185.SS', 'yahoo',
-                      dt.datetime(2015, 1, 1), dt.datetime(2015, 12, 31))
-
+wanke = web.DataReader('000002.SZ', 'yahoo', dt.datetime(2015, 1, 1), dt.datetime(2015, 12, 31))
+gldc = web.DataReader('600185.SS', 'yahoo', dt.datetime(2015, 1, 1), dt.datetime(2015, 12, 31))
 price = pd.concat([wanke.Close, gldc.Close], 1)
-
 price.columns = ['wanke', 'geli']
-
 ret = (price - price.shift(1)) / price.shift(1)
-
 model = smf.ols('wanke~geli', data=ret).fit()
-
 print(model.summary())
 
 # 5.
-
-zyhy = pd.read_table('Data/Part3/004/problem21.txt',
-                     sep='\t', usecols=['zyhy', 'Date'], index_col='Date')
+zyhy = pd.read_table(Path2+'/Part3/004/problem21.txt',sep='\t', usecols=['zyhy', 'Date'], index_col='Date')
 zyhy.index = pd.to_datetime(zyhy.index)
 
 ret = (zyhy - zyhy.shift(1)) / zyhy.shift(1)
 
-ThreeFactors = pd.read_table('Data/Part3/004/ThreeFactors.txt',
-                             sep='\t', index_col='TradingDate')
+ThreeFactors = pd.read_table(Path2+'/Part3/004/ThreeFactors.txt',sep='\t', index_col='TradingDate')
 ThreeFactors.index = pd.to_datetime(ThreeFactors.index)
 ThrFac = ThreeFactors['2014']
+ThrFac.columns
 ThrFac = ThrFac.iloc[:, [2, 4, 6]]
+dat = pd.concat([ret, ThrFac], 1).dropna()
 dat = pd.concat([ret, ThrFac], 1)
-
 model = smf.ols('zyhy~RiskPremium2+SMB2+HML2', data=dat).fit()
 print(model.summary())
+myDA.ThreeFactors(dat.zyhy,dat.RiskPremium2,dat.SMB2,dat.HML2,False)
 
 # 6.
-zhongxin = pd.read_table('Data/Part3/004/problem21.txt',
-                         sep='\t', usecols=['zhongxin', 'Date'], index_col='Date')
-
+zhongxin = pd.read_table(Path2+'/Part3/004/problem21.txt',sep='\t', usecols=['zhongxin', 'Date'], index_col='Date')
 zhongxin.index = pd.to_datetime(zhongxin.index)
-
 ret = (zhongxin - zhongxin.shift(1)) / zhongxin.shift(1)
 
-ThreeFactors = pd.read_table('Data/Part3/004/ThreeFactors.txt',
-                             sep='\t', index_col='TradingDate')
-
+ThreeFactors = pd.read_table(Path2+'/Part3/004/ThreeFactors.txt',sep='\t', index_col='TradingDate')
 ThreeFactors.index = pd.to_datetime(ThreeFactors.index)
-
 ThrFac = ThreeFactors['2014']
 ThrFac = ThrFac.iloc[:, [2, 4, 6]]
 dat = pd.concat([ret, ThrFac], 1)
@@ -90,17 +75,16 @@ model2 = smf.ols('zhongxin~RiskPremium2+SMB2+HML2', data=dat).fit()
 print(model2.summary())
 
 ThrFac = ThreeFactors['2015-01']
-preCAPM = model.predict(add_constant(ThrFac.RiskPremium2),
-                        transform=False)
-preFactors = model2.predict(add_constant(ThrFac[['RiskPremium2', 'SMB2', 'HML2']]),
-                            transform=False)
+preCAPM = model.predict(add_constant(ThrFac.RiskPremium2),transform=False)
+preFactors = model2.predict(add_constant(ThrFac[['RiskPremium2', 'SMB2', 'HML2']]),transform=False)
+preCAPM.plot()
+preFactors.plot()
+plt.show()
 
 # 7.
-import pandas as pd
+codes = pd.read_csv(Path2+'/Part3/004/codes.csv', header=None, dtype=str)
 
-codes = pd.read_csv('Data/Part3/004/codes.csv', header=None, dtype=str)
-
-ThreeFactors = pd.read_table('Data/Part3/004/ThreeFactors.txt', sep='\t', index_col='TradingDate')
+ThreeFactors = pd.read_table(Path2+'/Part3/004/ThreeFactors.txt', sep='\t', index_col='TradingDate')
 
 ThreeFactors.index = pd.to_datetime(ThreeFactors.index)
 
@@ -111,22 +95,15 @@ ThrFac = ThrFac.iloc[:, [2, 4, 6]]
 
 def create_func(model):
     def cal_alpha(code, model_name=model):
-        price = web.DataReader(code, 'yahoo',
-                               dt.datetime(2014, 1, 1), dt.datetime(2014, 12, 31)).Close
-
+        price = web.DataReader(code, 'yahoo', dt.datetime(2014, 1, 1), dt.datetime(2014, 12, 31)).Close
         ret = (price - price.shift(1)) / price.shift(1)
-
         ret.name = 'ret'
-
         dat = pd.concat([ret, ThrFac], 1)
-
         if model_name == 'CAPM':
             model = smf.ols('ret~RiskPremium2', data=dat).fit()
         elif model_name == 'factors':
             model = smf.ols('ret~RiskPremium2+SMB2+HML2', data=dat).fit()
-
         return (model.params[0])
-
     return (cal_alpha)
 
 
