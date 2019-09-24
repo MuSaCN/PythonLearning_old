c9aa0938875959526cf607344c1094a8fbf76400
MuSaCN <435116098@qq.com>
2019-09-24 18:59:46 +0800

    update

--- My_Python_Items/AutoMyPackageZip.py
+++ My_Python_Items/AutoMyPackageZip.py
@@ -5,7 +5,8 @@ import MyPackage
 __mypath__ = MyPackage.MyClass_Path.MyClass_Path()  #路径类
 myfile = MyPackage.MyClass_File.MyClass_File()  #文件操作类
 
-MyPackage_PathList = __mypath__.GetMyPackagePath()
+# MyPackage_PathList = __mypath__.GetMyPackagePath()
+MyPackage_PathList = ["C:\\Users\\i2011\\AppData\\Local\\Programs\\Python\\Python37\\lib\\site-packages\\MyPackage"]
 
 # ---备份到桌面
 myfile.ZipDir(MyPackage_PathList[0], zipPath="Desktop" , zipName=None, autoName=True)
