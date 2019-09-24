2e81247d97825c712c6bc28cdfb4b2eb6c7031e0
MuSaCN <435116098@qq.com>
2019-09-24 18:24:10 +0800

    update

--- My_Python_Items/PythonToWin.py
+++ My_Python_Items/PythonToWin.py
@@ -1,17 +1,15 @@
 # Author:Zhang Yuan
+# *.Py 转成 win的 *.exe文件
+# 安装pyinstaller
+    # 打开：View-Tool Windows-Terminal
+    # 输入：pip install pyinstaller
 
-file = "C:\\Users\\i2011\\PycharmProjects\\PythonLearning\\My_Python_Items\\AutoMyPackageZip.py"
-
-from distutils.core import setup
-import py2exe
-
-options = {"py2exe": {"optimize": 2}}
-setup(
-    options=options,
-    zipfile=None,
-    console=[file],
-     )
-
+# 生成exe文件
+    # Terminal中输入：“pyinstaller -F -w *.py” 就可以制作出exe。
+    # 注：星号用相应的文件名进行替换即可。
+    # 　　生成的文件放在同目录dist下。
+    # 　　-F（注意大写）是所有库文件打包成一个exe；
+    # 　　-w是不出黑色控制台窗口。
 
 
 
